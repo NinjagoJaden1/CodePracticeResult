bfe0c37efd2ad02be69bf40502c05607806ebfd6
NinjagoJaden1 <68250411+NinjagoJaden1@users.noreply.github.com>
2021-11-29 17:25:04 -0800

    Create Nesting Patterns.swift

---
+++ Jaden/learn code 1/Nesting Patterns.swift
@@ -0,0 +1,19 @@
+func turnAround(){
+    turnLeft()
+    turnLeft()
+}
+
+
+func solveStair(){
+    moveForward()
+    collectGem()
+    turnAround()
+    moveForward()
+}
+
+
+solveStair()
+solveStair()
+turnLeft()
+solveStair()
+solveStair()
